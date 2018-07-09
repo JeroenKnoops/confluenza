0b2d3a5464683a58582e67fdb62c561e6c106a55
Marcin Czenko <marcin@everydayproductive.com>
2018-07-09 15:55:07 +0200

    Gatsby2: Updates Layout to the new format

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,6 @@
+import { React } from 'react'
 import 'prismjs/themes/prism-coy.css'
-import graphql from 'graphql'
+import { StaticQuery, graphql } from 'gatsby'
 import glamorous from 'glamorous'
 import { rhythm } from '../utils/typography'
 
@@ -9,9 +10,7 @@ import { Navigation } from './Navigation'
 export const FixedNavigation = glamorous.div({
   display: 'block',
   position: 'fixed',
-  top: 0,
-  minWidth: '300px',
-  maxWidth: '300px',
+  top: rhythm(1),
   height: `calc(100vh - ${rhythm(2)})`,
   overflowY: 'auto',
   backgroundColor: '#70a8a9',
@@ -26,38 +25,44 @@ export const FixedNavigation = glamorous.div({
   }
 })
 
-const Layout = ({ children, data: { navigation: { docs } } }) => (
-  <LayoutGrid>
-    <SidebarGridItem>
-      <FixedNavigation>
-        <Navigation docs={docs} />
-      </FixedNavigation>
-    </SidebarGridItem>
-    <ContentGridItem>
-      { children() }
-    </ContentGridItem>
-  </LayoutGrid>
-)
-
-export const pageQuery = graphql`
-  query Navigation {
-    navigation: allMarkdownRemark(
-      filter: { frontmatter: { path: { ne: "/404.html" } } }
-      sort: { fields: [fileAbsolutePath], order: ASC }
-    ) {
-      docs: edges {
-        node {
-          frontmatter {
-            title
-            path
-          }
-          headings(depth: h2) {
-            value
+const Layout = ({ children, location }) => (
+  <StaticQuery
+    query={graphql`
+      query Navigation {
+        navigation: allMarkdownRemark(
+          filter: { frontmatter: { path: { ne: "/404.html" } } }
+          sort: { fields: [fileAbsolutePath], order: ASC }
+        ) {
+          docs: edges {
+            node {
+              frontmatter {
+                title
+                path
+              }
+              headings(depth: h2) {
+                value
+              }
+            }
           }
         }
       }
-    }
-  }
-`
+    `}
+    render={data => {
+      const { navigation: { docs } } = data
+      return (
+        <LayoutGrid>
+          <SidebarGridItem>
+            <FixedNavigation>
+              <Navigation docs={docs} />
+            </FixedNavigation>
+          </SidebarGridItem>
+          <ContentGridItem>
+            { children }
+          </ContentGridItem>
+        </LayoutGrid>
+      )
+    }}
+  />
+)
 
 export default Layout
