1a4ddfa56166ed6784a3ef33891fca4cd04ecd4b
Marcin Czenko <marcin@everydayproductive.com>
2018-07-09 15:57:40 +0200

    Gatsby2: updates markdownTemplate to use new Layout

--- src/templates/markdownTemplate.js
+++ src/templates/markdownTemplate.js
@@ -1,19 +1,30 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import { EditFile } from '../components/Editing'
-import graphql from 'graphql'
+import glamorous from 'glamorous'
+import { graphql } from 'gatsby'
+
+import Layout from '../components/Layout'
 
 const Template = ({ data: { doc } }) => {
   const { html, fileAbsolutePath, frontmatter: { title } } = doc
   return (
-    <div>
+    <Layout>
       <Helmet title={title} />
-      <EditFile fileAbsolutePath={fileAbsolutePath} />
-      <h1>{title}</h1>
+      <Header>
+        <h1>{title}</h1>
+        <EditFile fileAbsolutePath={fileAbsolutePath} />
+      </Header>
       <div dangerouslySetInnerHTML={{ __html: html }} />
-    </div>
+    </Layout>
   )
 }
+
+const Header = glamorous.div({
+  display: 'flex',
+  flexFlow: 'row nowrap',
+  justifyContent: 'space-between'
+})
 
 export const pageQuery = graphql`
   query MarkdownByPath($path: String!) {
